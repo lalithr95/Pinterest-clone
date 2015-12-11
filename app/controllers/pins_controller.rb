42e32053f63d8cd294a19ed695342f387e9bd08d
Lalith <lalithr95@gmail.com>
2015-12-11 16:29:54 +0530

    Add authentication for pins

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,15 +1,16 @@
 class PinsController < ApplicationController
 	before_action :find_pin, only: [:show, :edit, :update, :destroy]
+	before_action :authenticate_user!
 	def index
 		@pins = Pin.all.order("created_at DESC")
 	end
 
 	def new
-		@pin = Pin.new
+		@pin = current_user.pins.build
 	end
 
 	def create
-		@pin = Pin.new(pin_params)
+		@pin = current_user.pins.build(pin_params)
 		if @pin.save
 			redirect_to @pin, notice: "Successfully created pin"
 		else

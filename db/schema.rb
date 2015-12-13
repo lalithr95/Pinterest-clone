85cc14494e5d6b536a493c265a4521c3e2854420
Lalith <lalithr95@gmail.com>
2015-12-14 02:19:10 +0530

    schema update after migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151212102618) do
+ActiveRecord::Schema.define(version: 20151213204801) do
+
+  create_table "comments", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "pin_id"
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "pins", force: :cascade do |t|
     t.string   "title"

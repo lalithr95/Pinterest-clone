e0e93c4b2cfb760f6b8723433855e5dec4df657f
Lalith <lalithr95@gmail.com>
2015-12-13 16:53:37 +0530

    fix for end brace and reduce pixel

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,6 +1,6 @@
 class Pin < ActiveRecord::Base
 	belongs_to :user
-	has_attached_file  :image, styles: { medium: "300x300>" }
+	has_attached_file  :image, styles: { medium: "100x100>" }
 	validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 	acts_as_votable
 end
